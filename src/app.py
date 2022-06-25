b1c52681784aa31cf72b1e8d6ab3aa6d73b89364
sevys <201228@ids.upchiapas.edu.mx>
2022-06-25 18:06:34 -0500

    DIN-13: subiendo el archivo de configuracion

--- src/app.py
+++ src/app.py
@@ -1,9 +1,24 @@
 from flask import Flask
 from config import config
+from flask_mysqldb import MySQL
+
 
 
 app = Flask(__name__)
 
+conexion = MySQL(app)
+
+
+@app.route('/products')
+def get_products():
+    try:
+        return 'ok'
+
+    except Exception as ex:
+        return 'error ', ex
+
+
 if __name__ == '__main__':
     app.config.from_object(config['development'])
     app.run()
+ 
--- src/config.py
+++ src/config.py
@@ -1,9 +1,18 @@
 from distutils.debug import DEBUG
+import pymysql
 
 
 class DevelopmentConfig():
     DEBUG = True
-
+   
 config = {
     'development': DevelopmentConfig
 }
+
+def obtener_conexion():
+    return pymysql.connect(
+        host='localhost',
+        user='root',
+        password='01Hachico',
+        db='mydb'
+    )
